cbbc595ed7a97afd8042510fc649fa964d0aaa62
TahirReyaz <tahirreyaz5@gmail.com>
2024-01-18 13:13:52 +0530

    Added placeholder poster

--- src/components/search/ResultItem.tsx
+++ src/components/search/ResultItem.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { mediaTypeType } from "../../constants/types";
 import { Link } from "react-router-dom";
 
+import posterPlaceholder from "../../assets/posterPlaceholder.jpg";
+
 interface ResultItemProps {
   type: mediaTypeType;
   title: string;
@@ -14,9 +16,15 @@ const ResultItem = ({ type, title, time, poster, url }: ResultItemProps) => {
   return (
     <Link to={url} className="flex py-3 px-4 hover:bg-actionPrimary">
       <img
-        src={`${import.meta.env.VITE_TMDB_IMG_ENDPOINT}${poster}`}
+        src={
+          poster
+            ? `${import.meta.env.VITE_TMDB_IMG_ENDPOINT}${poster}`
+            : posterPlaceholder
+        }
         alt={title}
-        className="rounded aspect-square object-cover object-top w-1/12"
+        className={`rounded aspect-square object-cover ${
+          type == "person" ? "object-center" : "object-top"
+        } w-1/12`}
       />
       <div className="ms-3 text-textPrimary">{title}</div>
     </Link>
