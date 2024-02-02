1d83632667f6f72e09974f50c7da0ef5b61fd171
TahirReyaz <tahirreyaz5@gmail.com>
2024-02-02 14:43:42 +0530

    Update react query dev tools look

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/AuthSlice.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,14 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         <App />
+        {/* I am cloning Anilist.co and they use fontSize: 10px which I have included in the <html> tag. So that font-size is scaling down tanstack dev tools thats why standard font-size is provided in wrapper div of tanstack dev tools*/}
+        <div
+          style={{
+            fontSize: 16,
+          }}
+        >
+          <ReactQueryDevtools />
+        </div>
       </Provider>
     </QueryClientProvider>
   </React.StrictMode>
