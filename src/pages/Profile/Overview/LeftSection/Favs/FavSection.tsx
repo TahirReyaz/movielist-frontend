69060db5c8e4fdd4e99b0616dea6af59b5713f41
TahirReyaz <tahirreyaz5@gmail.com>
2024-04-21 19:53:22 +0530

    Small UI Fix

--- src/pages/Profile/Overview/LeftSection/Favs/FavSection.tsx
+++ src/pages/Profile/Overview/LeftSection/Favs/FavSection.tsx
@@ -8,8 +8,8 @@ interface FavSectionProps {
 }
 const FavSection = ({ title, type, list }: FavSectionProps) => {
   return (
-    <div>
-      <h2 className="text-xl font-semibold">{title}</h2>
+    <div className="my-12">
+      <h2 className="text-xl font-semibold ms-4 mb-4">{title}</h2>
       <ul className="bg-bgSecondary rounded p-4 grid grid-cols-4">
         {list.map((item) => (
           <FavItem {...{ id: item, type, key: item }} />
