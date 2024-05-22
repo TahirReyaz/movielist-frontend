e982894a9a106facf2570f9d366b233678ce52b1
TahirReyaz <tahirreyaz5@gmail.com>
2024-05-22 12:35:45 +0530

    Hide the filters on mobile

--- src/pages/Profile/MediaList/LeftSection.tsx/index.tsx
+++ src/pages/Profile/MediaList/LeftSection.tsx/index.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { HiDotsHorizontal } from "react-icons/hi";
+
 import TextInput from "../../../../components/UI/TextInput";
 import Lists from "./Lists";
 
@@ -8,28 +10,38 @@ interface LeftSectionProps {
 
 const LeftSection = ({ allowedList }: LeftSectionProps) => {
   const [search, setSearch] = useState("");
+  const [show, setShow] = useState(false);
 
   return (
     <div className="mt-12">
-      <TextInput
-        {...{
-          value: search,
-          onChange: (e) => setSearch(e.target.value),
-          name: "search",
-          type: "text",
-          label: "Filter",
-          classes: "bg-bgSecondary",
-        }}
-      />
-      <Lists
-        {...{
-          val: allowedList,
-        }}
-      />
-      <div>Filters</div>
-      <div>Year</div>
-      <div>Sort</div>
-      <div>Button</div>
+      <div className="grid grid-cols-12 gap-4">
+        <TextInput
+          {...{
+            value: search,
+            onChange: (e) => setSearch(e.target.value),
+            name: "search",
+            type: "text",
+            label: "Filter",
+            classes: "bg-bgSecondary",
+            divClasses: "col-span-10 md:col-span-12",
+          }}
+        />
+        <HiDotsHorizontal
+          className="md:hidden col-span-2 md:col-span-0 bg-bgSecondary rounded p-2 text-3xl"
+          onClick={() => setShow((prev) => !prev)}
+        />
+      </div>
+      <div className={`${!show && "hidden"} md:block`}>
+        <Lists
+          {...{
+            val: allowedList,
+          }}
+        />
+        <div>Filters</div>
+        <div>Year</div>
+        <div>Sort</div>
+        <div>Button</div>
+      </div>
     </div>
   );
 };
