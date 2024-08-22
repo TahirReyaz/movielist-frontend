f2308e63023d0f77d78259e94f303565fb28ef4e
TahirReyaz <tahirreyaz5@gmail.com>
2024-08-22 16:59:09 +0530

    Bug fix

--- src/pages/MediaDetail/Pages/Characters/index.tsx
+++ src/pages/MediaDetail/Pages/Characters/index.tsx
@@ -6,7 +6,7 @@ import { getMediaMoreDetails } from "../../../../lib/api/media";
 import Error from "../../../../components/UI/Error";
 import CharacterCard from "../Overview/Characters/CharacterCard";
 import { useLocation, useParams } from "react-router-dom";
-import { MediaCredits } from "../../../../constants/types/media";
+import { CastMember, MediaCredits } from "../../../../constants/types/media";
 
 const Characters = () => {
   const { mediaid } = useParams();
@@ -36,8 +36,8 @@ const Characters = () => {
       <h2 className="text-[1.4rem] text-right font-semibold my-4">Dropdown</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-        {credits?.characters?.map((char: any) => (
-          <CharacterCard {...{ key: char.id, char }} />
+        {credits?.characters?.map((char: CastMember) => (
+          <CharacterCard {...{ key: char.id, ...char }} />
         ))}
       </div>
     </div>
--- src/pages/MediaDetail/Pages/Overview/Characters/CharacterCard.tsx
+++ src/pages/MediaDetail/Pages/Overview/Characters/CharacterCard.tsx
@@ -5,15 +5,10 @@ import userAvatar from "../../../../../assets/userAvatar.png";
 import { Link } from "react-router-dom";
 import { CastMember } from "../../../../../constants/types/media";
 
-const CharacterCard = ({
-  cast_id,
-  character,
-  name,
-  profile_path,
-}: CastMember) => {
+const CharacterCard = ({ id, character, name, profile_path }: CastMember) => {
   return (
     <div className="grid grid-cols-6 rounded overflow-hidden bg-bgForeground">
-      <Link to={`/staff/${cast_id}`}>
+      <Link to={`/staff/${id}`}>
         <img
           src={profile_path ? `${tmdbImgEndPoint}${profile_path}` : userAvatar}
           alt={name}
@@ -22,7 +17,7 @@ const CharacterCard = ({
       <div className="col-span-5 p-4 flex flex-col items-start justify-between">
         <p className="text-xl">{character}</p>
         <Link
-          to={`/staff/${cast_id}`}
+          to={`/staff/${id}`}
           className="text-xl hover:text-actionPrimary grid justify-end"
         >
           {name}
