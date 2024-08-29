bb9470318f31450d58750196cd26b6424ae82db3
TahirReyaz <tahirreyaz5@gmail.com>
2024-08-29 11:24:58 +0530

    Showing warning modal before logout in user drop down menu

--- src/Layout/Navbar/UserDropdownMenu.tsx
+++ src/Layout/Navbar/UserDropdownMenu.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   FaAppStoreIos,
   FaDiscord,
@@ -11,17 +11,23 @@ import { FaGear } from "react-icons/fa6";
 import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 
-import { RootState } from "../../store";
 import { logoutAction } from "../../store/AuthSlice";
 import DropdownMenu from "./DropdownMenu";
 import { attrsType } from "./BrowseDropdownMenu";
 import { useAppSelector } from "../../hooks/redux";
+import WarningModal from "../../components/UI/WarningModal";
 
 const UserDropdownMenu = ({ attrs }: { attrs: attrsType }) => {
+  const [openWarningModal, setOpenWarningModal] = useState<boolean>(false);
   const { username } = useAppSelector((state) => state.auth);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(logoutAction());
+    navigate("/");
+  };
 
   const primaryLinks = [
     {
@@ -43,50 +49,59 @@ const UserDropdownMenu = ({ attrs }: { attrs: attrsType }) => {
   const footer = [
     {
       title: "Donate",
-      action: () => {},
+      action: () => navigate("/donate"),
       icon: <FaHeart />,
     },
     {
       title: "Apps",
-      action: () => {},
+      action: () => navigate("/apps"),
       icon: <FaAppStoreIos />,
     },
     { title: "Discord", action: () => {}, icon: <FaDiscord /> },
     {
       title: "Logout",
-      action: () => {
-        dispatch(logoutAction());
-        navigate("/");
-      },
+      action: () => setOpenWarningModal(true),
       icon: <FaSignOutAlt />,
     },
   ];
 
   return (
-    <DropdownMenu
-      {...{
-        mainContent: primaryLinks.map((link) => (
-          <Link
-            key={link.title}
-            to={link.url}
-            className="flex hover:text-textBright mb-2 text-2xl font-bold items-center"
-          >
-            {link.icon}
-            <div className="ms-8 font-bold">{link.title}</div>
-          </Link>
-        )),
-        footerContent: footer.map((link) => (
-          <div
-            key={link.title}
-            className="w-4/12 hover:text-textBright flex items-center text-lg"
-            onClick={link.action}
-          >
-            {link.icon} <span className="ms-2 font-medium">{link.title}</span>
-          </div>
-        )),
-        attrs,
-      }}
-    />
+    <>
+      <DropdownMenu
+        {...{
+          mainContent: primaryLinks.map((link) => (
+            <Link
+              key={link.title}
+              to={link.url}
+              className="flex hover:text-textBright mb-2 text-2xl font-bold items-center"
+            >
+              {link.icon}
+              <div className="ms-8 font-bold">{link.title}</div>
+            </Link>
+          )),
+          footerContent: footer.map((link) => (
+            <div
+              key={link.title}
+              className="w-4/12 hover:text-textBright flex items-center text-lg"
+              onClick={link.action}
+            >
+              {link.icon} <span className="ms-2 font-medium">{link.title}</span>
+            </div>
+          )),
+          attrs,
+        }}
+      />
+      <WarningModal
+        {...{
+          open: openWarningModal,
+          setOpen: setOpenWarningModal,
+          action: handleLogout,
+          title: "Warning",
+          message: "Are you sure you want to Log out?",
+          actionName: "OK",
+        }}
+      />
+    </>
   );
 };
 
