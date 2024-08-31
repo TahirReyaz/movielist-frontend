fd1ed9cf84ffe2bafcdb5cf4c9817dafc456790f
TahirReyaz <tahirreyaz5@gmail.com>
2024-08-31 16:33:25 +0530

    Small fix

--- src/pages/Home/RightSection/TrendingMedia/index.tsx
+++ src/pages/Home/RightSection/TrendingMedia/index.tsx
@@ -5,6 +5,7 @@ import { getBulkMedia } from "../../../../lib/api";
 import Loading from "../../../../components/UI/Loading";
 import { MovieDetail } from "../../../../constants/types/media";
 import TrendingMediaCard from "../../../../components/Media/TrendingMediaCard";
+import { Link } from "react-router-dom";
 
 const TrendingMedia = () => {
   const {
@@ -25,9 +26,11 @@ const TrendingMedia = () => {
 
   return (
     <div className="hidden md:block">
-      <h2 className="text-[1.4rem] font-medium px-4 py-2 hover:text-actionPrimary cursor-pointer">
-        Trending Movie and TV
-      </h2>
+      <Link to={`/search/movie/trending`}>
+        <h2 className="text-[1.4rem] font-medium px-4 py-2 hover:text-actionPrimary cursor-pointer">
+          Trending Movie and TV
+        </h2>
+      </Link>
       <div className="grid grid-cols-4 gap-8 rounded-lg bg-anilist-mirage p-4 mb-4 shadow-lg">
         {media &&
           media
