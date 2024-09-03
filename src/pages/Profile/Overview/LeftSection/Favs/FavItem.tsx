4cdfb9aa8e7b7d71c7a9ef9d7ac48bfa6ee503f1
TahirReyaz <tahirreyaz5@gmail.com>
2024-09-03 16:46:04 +0530

    Show item name on fav hover

--- src/pages/Profile/Overview/LeftSection/Favs/FavItem.tsx
+++ src/pages/Profile/Overview/LeftSection/Favs/FavItem.tsx
@@ -7,6 +7,7 @@ import { getMediaDetail } from "../../../../../lib/api";
 import { getStaffDetails } from "../../../../../lib/api/staff";
 import { posterSizes, tmdbImgBaseUrl } from "../../../../../constants/tmdb";
 import { Link } from "react-router-dom";
+import Tippy from "@tippyjs/react";
 
 interface FavItem {
   id: number;
@@ -32,20 +33,28 @@ const FavItem = ({ id, type }: FavItem) => {
   }
 
   return (
-    <Link to={`/${type}/${id}`} className="rounded">
-      <img
-        src={
-          data.profile_path || data.poster_path
-            ? `${tmdbImgBaseUrl}/${posterSizes.sm}${
-                type === "staff" ? data.profile_path : data.poster_path
-              }`
-            : posterPlaceholder
-        }
-        alt={title}
-        title={title}
-        className="rounded"
-      />
-    </Link>
+    <Tippy
+      {...{
+        content: title,
+        trigger: "mouseenter focus",
+        interactive: false,
+        arrow: true,
+      }}
+    >
+      <Link to={`/${type}/${id}`} className="rounded">
+        <img
+          src={
+            data.profile_path || data.poster_path
+              ? `${tmdbImgBaseUrl}/${posterSizes.sm}${
+                  type === "staff" ? data.profile_path : data.poster_path
+                }`
+              : posterPlaceholder
+          }
+          alt={title}
+          className="rounded"
+        />
+      </Link>
+    </Tippy>
   );
 };
 
