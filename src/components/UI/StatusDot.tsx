52342217e32c37c6315b88ed763f8f1cc275569f
TahirReyaz <tahirreyaz5@gmail.com>
2024-09-09 17:07:22 +0530

    Fixed status dot style

--- src/components/UI/StatusDot.tsx
+++ src/components/UI/StatusDot.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const StatusDot = ({ color }: { color?: string }) => {
   return (
     <span
-      className={`inline-block w-4 h-4 mr-4 rounded-full`}
-      style={{ backgroundColor: color }}
+      className={`inline-block w-2 h-2 mr-4 rounded-full`}
+      style={{ backgroundColor: color, boxShadow: `0 0 8px ${color}` }}
     />
   );
 };
