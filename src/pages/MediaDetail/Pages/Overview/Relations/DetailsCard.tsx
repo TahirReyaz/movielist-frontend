14688f9db649dd2373a62d49679f0521667a4ab3
TahirReyaz <tahirreyaz5@gmail.com>
2024-09-10 12:09:04 +0530

    Small UI fix

--- src/pages/MediaDetail/Pages/Overview/Relations/DetailsCard.tsx
+++ src/pages/MediaDetail/Pages/Overview/Relations/DetailsCard.tsx
@@ -10,7 +10,7 @@ const DetailsCard = ({
   const releaseYear = release_date ? new Date(release_date).getFullYear() : "";
   return (
     <div className="flex flex-col justify-between bg-anilist-mirage rounded-e-md p-4 h-full">
-      <h2 className="text-2xl">{title}</h2>
+      <h2 className="text-2xl text-anilist-blue-picton font-medium">{title}</h2>
       <p className="text-xl font-light">{releaseYear}</p>
     </div>
   );
