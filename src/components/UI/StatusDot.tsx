65601806714d4247f35a0f771c39fa719a9987c8
TahirReyaz <tahirreyaz5@gmail.com>
2024-09-11 17:00:33 +0530

    Small UI fix

--- src/components/UI/StatusDot.tsx
+++ src/components/UI/StatusDot.tsx
@@ -4,7 +4,9 @@ const StatusDot = ({ color }: { color?: string }) => {
   return (
     <span
       className={`inline-block w-2 h-2 mr-4 rounded-full`}
-      style={{ backgroundColor: color, boxShadow: `0 0 8px ${color}` }}
+      style={
+        color ? { backgroundColor: color, boxShadow: `0 0 8px ${color}` } : {}
+      }
     />
   );
 };
